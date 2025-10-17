30ed290ac6ff5b9f5d8733d75f36e84e2006cfbd
Younes <oussamatrix96@gmail.com>
2025-10-17 12:29:40 +0100

    Resolve merge conflict

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,19 +1,16 @@
 // Test ID: IIDSAT
 
-<<<<<<< HEAD
 import OrderItem from './OrderItem';
 
 import { useLoaderData } from 'react-router-dom';
-=======
->>>>>>> recovery
+
 import { getOrder } from '../../services/apiRestaurant';
 import {
   calcMinutesLeft,
   formatCurrency,
   formatDate,
 } from '../../utils/helpers';
-<<<<<<< HEAD
-=======
+
 import OrderItem from './OrderItem';
 
 const order = {
@@ -51,7 +48,6 @@ const order = {
   priorityPrice: 19,
   status: 'Delivered',
 };
->>>>>>> recovery
 
 function Order() {
   const order = useLoaderData();
@@ -66,27 +62,7 @@ function Order() {
     estimatedDelivery,
     cart,
   } = order;
-<<<<<<< HEAD
 
-  const deliveryIn = calcMinutesLeft(
-    estimatedDelivery,
-  );
-
-  return (
-    <div className="space-y-8 px-4 py-6">
-      <div className="flex flex-wrap items-center justify-between gap-2">
-        <h2 className="text-xl font-semibold">
-          Order #{id} status
-        </h2>
-
-        <div className="space-x-2">
-          {priority && (
-            <span className="rounded-full bg-red-500 px-3 py-1 text-sm font-semibold uppercase tracking-wide text-red-50">
-              Priority
-            </span>
-          )}
-          <span className="rounded-full bg-green-500 px-3 py-1 text-sm font-semibold uppercase tracking-wide text-green-50">
-=======
   const deliveryIn = calcMinutesLeft(
     estimatedDelivery,
   );
@@ -107,19 +83,13 @@ function Order() {
             </span>
           )}
           <span className="rounded-full bg-green-500 px-3 py-1 text-sm font-semibold uppercase text-stone-100">
->>>>>>> recovery
             {status} order
           </span>
         </div>
       </div>
 
-<<<<<<< HEAD
-      <div className="flex flex-wrap items-center justify-between gap-2 bg-stone-200 px-6 py-5">
-        <p className="font-medium">
-=======
       <div className="flex flex-wrap items-center justify-between gap-2 bg-stone-200 px-4 py-6">
         <p className="text-base font-semibold text-stone-700">
->>>>>>> recovery
           {deliveryIn >= 0
             ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃`
             : 'Order should have arrived'}
@@ -130,16 +100,6 @@ function Order() {
         </p>
       </div>
 
-<<<<<<< HEAD
-      <ul className="dive-stone-200 divide-y border-b border-t">
-        {cart.map((item) => (
-          <OrderItem item={item} key={item.id} />
-        ))}
-      </ul>
-
-      <div className="space-y-2 bg-stone-200 px-6 py-5">
-        <p className="text-sm font-medium text-stone-600">
-=======
       <ul>
         {cart.map((item) => (
           <OrderItem
@@ -152,25 +112,17 @@ function Order() {
 
       <div className="space-y-1 bg-stone-200 px-4 py-6">
         <p className="text-sm font-semibold text-stone-600">
->>>>>>> recovery
           Price pizza:{' '}
           {formatCurrency(orderPrice)}
         </p>
         {priority && (
-<<<<<<< HEAD
-          <p className="text-sm font-medium text-stone-600">
-=======
           <p className="text-sm font-semibold text-stone-600">
->>>>>>> recovery
             Price priority:{' '}
             {formatCurrency(priorityPrice)}
           </p>
         )}
-<<<<<<< HEAD
-        <p className="font-bold">
-=======
+
         <p className="text-lg font-bold text-stone-700">
->>>>>>> recovery
           To pay on delivery:{' '}
           {formatCurrency(
             orderPrice + priorityPrice,
